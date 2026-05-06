f6122264381717b6807d23f5126abfbf02938acd
sohamkoli29 <sohamkoli29@gmaol.com>
2026-05-06 15:42:41 +0530

    basic ui

--- babel.config.js
+++ babel.config.js
@@ -2,10 +2,8 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: [
-      ['babel-preset-expo', { 
-        jsxImportSource: 'nativewind',
-        unstable_transformProfile: 'hermes-stable' // add this
-      }],
+      ['babel-preset-expo', { jsxImportSource: 'nativewind' }],
+      'nativewind/babel',
     ],
   };
 };
